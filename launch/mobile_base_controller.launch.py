424c14b8473928fbd17e5a48d9c0dc1ef4a4216d
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-09-23 15:32:00 +0800

    Rebase from 'debian/humble/pmb2_controller_configuration'

--- launch/mobile_base_controller.launch.py
+++ launch/mobile_base_controller.launch.py
@@ -18,15 +18,16 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from controller_manager.launch_utils import generate_load_controller_launch_description
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import GroupAction
-from launch_pal.arg_utils import LaunchArgumentsBase
+from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
+from launch.actions import SetLaunchConfiguration
+from launch.substitutions import LaunchConfiguration
 from launch_pal.robot_arguments import CommonArgs
-from launch_pal.include_utils import include_scoped_launch_py_description
+from launch_pal.arg_utils import LaunchArgumentsBase, read_launch_argument
 
 
 @dataclass(frozen=True)
 class LaunchArguments(LaunchArgumentsBase):
+    use_sim_time: DeclareLaunchArgument = CommonArgs.use_sim_time
     is_public_sim: DeclareLaunchArgument = CommonArgs.is_public_sim
 
 
@@ -47,40 +48,32 @@ def declare_actions(
     launch_description: LaunchDescription, launch_args: LaunchArguments
 ):
 
-    pkg_name = 'pmb2_controller_configuration'
-    pkg_share_folder = get_package_share_directory(pkg_name)
+    launch_description.add_action(OpaqueFunction(
+        function=set_base_config_file))
 
     # Base controller
-    base_controller = include_scoped_launch_py_description(
-        pkg_name=pkg_name,
-        paths=['launch', 'mobile_base_controller.launch.py'],
-        launch_arguments={
-            'is_public_sim': launch_args.is_public_sim,
-        }
+    base_controller = GroupAction(
+        [
+            generate_load_controller_launch_description(
+                controller_name='mobile_base_controller',
+                controller_params_file=LaunchConfiguration("base_config_file")
+            )
+        ],
     )
     launch_description.add_action(base_controller)
-
-    # Joint state broadcaster
-    joint_state_broadcaster = GroupAction(
-        [
-            generate_load_controller_launch_description(
-                controller_name='joint_state_broadcaster',
-                controller_params_file=os.path.join(
-                    pkg_share_folder, 'config', 'joint_state_broadcaster.yaml'))
-        ],
-    )
-    launch_description.add_action(joint_state_broadcaster)
-
-    # IMU sensor broadcaster
-    imu_sensor_broadcaster = GroupAction(
-        [
-            generate_load_controller_launch_description(
-                controller_name='imu_sensor_broadcaster',
-                controller_params_file=os.path.join(
-                    pkg_share_folder, 'config', 'imu_sensor_broadcaster.yaml'))
-
-        ],
-    )
-    launch_description.add_action(imu_sensor_broadcaster)
-
     return
+
+
+def set_base_config_file(context):
+
+    is_public_sim = read_launch_argument("is_public_sim", context)
+    pkg_share_folder = get_package_share_directory('pmb2_controller_configuration')
+
+    controller_file = 'mobile_base_controller.yaml'
+
+    if is_public_sim in ['true', 'True']:
+        controller_file = 'mobile_base_controller_public_sim.yaml'
+
+    base_config_file = os.path.join(pkg_share_folder, 'config', controller_file)
+
+    return [SetLaunchConfiguration("base_config_file", base_config_file)]
